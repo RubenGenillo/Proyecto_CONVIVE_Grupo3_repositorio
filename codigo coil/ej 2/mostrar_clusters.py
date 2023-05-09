e1275d049588e9dbb76a0d0497e6b317f1d9fd0f
Jojo3235 <joralmand@gmail.com>
2023-05-09 21:06:46 +0200

    regresion

--- codigo coil/ej 2/mostrar_clusters.py
+++ codigo coil/ej 2/mostrar_clusters.py
@@ -4,31 +4,27 @@ import matplotlib.pyplot as plt
 
 # Cargar el csv
 df = pd.read_csv('codigo coil/clusterizado_1.csv', sep=';')
-df2 = pd.read_csv('codigo coil/clusterizado_2.csv', sep=';')
-df3 = pd.read_csv('codigo coil/clusterizado_3.csv', sep=';')
-df4 = pd.read_csv('codigo coil/clusterizado_4.csv', sep=';')
 
-# Mostrar los clusters en una matriz 2x2 de gráficos, dia frente a LAEQ
+# Gráfico de dispersión
 fig, axs = plt.subplots(2, 2)
 
-axs[0, 0].scatter(df['dia'], df['LAEQ'], c=df['cluster'])
-axs[0, 0].set_title('NMT1')
-axs[0, 0].set_xlabel('dia')
-axs[0, 0].set_ylabel('LAEQ')
+for i, ax in enumerate(axs.flatten()):
+    if i+1 > 4: break  # en caso de haber más de 4 archivos
+    dftemp = pd.read_csv(f'codigo coil/clusterizado_{i+1}.csv', sep=';')
+    ax.scatter(dftemp['dia'], dftemp['LAEQ'], c=dftemp['cluster'])
+    ax.set_title(f'NMT{i+1}')
+    ax.set_xlabel('dia')
+    ax.set_ylabel('LAEQ')
 
-axs[0, 1].scatter(df2['dia'], df2['LAEQ'], c=df2['cluster'])
-axs[0, 1].set_title('NMT2')
-axs[0, 1].set_xlabel('dia')
-axs[0, 1].set_ylabel('LAEQ')
+    # Línea de regresión
+    x = dftemp['dia']
+    y = dftemp['LAEQ']
+    coeffs = np.polyfit(x, y, 1)
+    poly_eqn = np.poly1d(coeffs)
+    ax.plot(x, poly_eqn(x), color='red')
 
-axs[1, 0].scatter(df3['dia'], df3['LAEQ'], c=df3['cluster'])
-axs[1, 0].set_title('NMT3')
-axs[1, 0].set_xlabel('dia')
-axs[1, 0].set_ylabel('LAEQ')
-
-axs[1, 1].scatter(df4['dia'], df4['LAEQ'], c=df4['cluster'])
-axs[1, 1].set_title('NMT4')
-axs[1, 1].set_xlabel('dia')
-axs[1, 1].set_ylabel('LAEQ')
+    # Coeficiente de correlación
+    corr = np.corrcoef(x, y)[0, 1]
+    ax.text(0.05, 0.95, f'Corr: {corr:.2f}', transform=ax.transAxes, ha='left', va='top')
 
 plt.show()
